67c2864269d6fbeb84c69ed25da73a633e4d49eb
SangEun Lee <d4space@gmail.com>
2014-03-26 21:06:10 +0100

    hahah

--- test/bsub.sh
+++ test/bsub.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-bsub -q 8nh -J job1SigGenCut <job1Sig_GenCut.sh
-bsub -q 8nh -J job1IntGen <job1Int_GenCut.sh
-bsub -q 8nh -J job1ContGen <job1Cont_GenCut.sh
+#bsub -q 8nh -J job1SigGenCut <job1Sig_GenCut.sh
+bsub -q 2nd -J SigGeCut1 <SigHw25RoScGeCut_1.sh
+bsub -q 2nd -J SigMiCut1 <SigHw25RoScMiCut_1.sh
+bsub -q 2nd -J ConGeCut1 <ContHw25RoScGeCut_1.sh
+bsub -q 2nd -J ConMiCut1 <ContHw25RoScMiCut_1.sh
+bsub -q 2nd -J IntGeCut1 <IntHw25RoScGeCut_1.sh
+bsub -q 2nd -J IntMiCut1 <IntHw25RoScMiCut_1.sh
